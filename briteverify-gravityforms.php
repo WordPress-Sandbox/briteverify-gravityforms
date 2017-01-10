3fcc8ea923a525d15bd4fa829af9dc0b635acc3f
David <david@shellcreeper.com>
2017-01-10 19:57:38 +0700

    add cons + change license file.

--- briteverify-gravityforms.php
+++ briteverify-gravityforms.php
@@ -1,6 +1,6 @@
 <?php
 /**
- * Plugin Name: BriteVerify GravityFOrms Add-On
+ * Plugin Name: BriteVerify GravityForms
  * Plugin URI: http://astoundify.com/downloads/briteverify-gravityforms/
  * Description: {SHORT DESCRIPTION}
  * Version: 1.0.0
@@ -12,3 +12,48 @@
  * Domain Path: /languages/
 **/
 if ( ! defined( 'WPINC' ) ) { die; }
+
+
+/* Constants
+------------------------------------------ */
+
+define( 'BV_GF_URI', trailingslashit( plugin_dir_url( __FILE__ ) ) );
+define( 'BV_GF_PATH', trailingslashit( plugin_dir_path( __FILE__ ) ) );
+define( 'BV_GF_FILE', __FILE__ );
+define( 'BV_GF_PLUGIN', plugin_basename( __FILE__ ) );
+define( 'BV_GF_VERSION', '1.0.0' );
+
+
+/* Init
+------------------------------------------ */
+
+/* Load plugin in "plugins_loaded" hook */
+add_action( 'plugins_loaded', 'bv_gf_init' );
+
+/**
+ * Plugin Init
+ * @since 0.1.0
+ */
+function bv_gf_init(){
+
+	
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
