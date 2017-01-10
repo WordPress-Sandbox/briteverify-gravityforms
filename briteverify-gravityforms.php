5e662a727275d3b4beed877a44395d150c355b2c
David <david@shellcreeper.com>
2017-01-10 08:12:51 +0700

    main plugin file.

---
+++ briteverify-gravityforms.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Plugin Name: BriteVerify GravityFOrms Add-On
+ * Plugin URI: http://astoundify.com/downloads/briteverify-gravityforms/
+ * Description: {SHORT DESCRIPTION}
+ * Version: 1.0.0
+ * Author: Astoundify
+ * Author URI: http://astoundify.com/
+ * License: GPLv2 or later
+ * License URI: http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain: briteverify-gravityforms
+ * Domain Path: /languages/
+**/
+if ( ! defined( 'WPINC' ) ) { die; }
