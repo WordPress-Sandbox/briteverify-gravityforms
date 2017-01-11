d7641e1a8690c06a6c005253a1860d860c945229
David <david@shellcreeper.com>
2017-01-11 13:06:49 +0700

    successfully save the advanced data in email field.

--- includes/field-settings.php
+++ includes/field-settings.php
@@ -45,21 +45,22 @@ class BV_GF_Field_Settings{
 			?>
 			<li class="bv_gf_enable_field_setting field_setting">
 				<label class="section_label"><?php esc_html_e( 'BriteVerify: Enable Verification', 'briteverify-gravityforms' ); ?> </label>
+				<p><span></p>
 				<div>
 
-					<input type="radio" name="bv_gf_enable" id="bv_gf_enable_default" size="10" value="default" />
+					<input type="radio" name="bv_gf_enable" id="bv_gf_enable_default" size="10" value="" onclick="return SetFieldProperty( 'bv_gf_enable', this.value );" onkeypress="return SetFieldProperty( 'bv_gf_enable', this.value );" />
 					<label for="bv_gf_enable_default" class="inline">
 						<?php printf( esc_html__( 'Default (%s)', 'briteverify-gravityforms' ), 1 == get_option( 'bv_gf_enable' ) ? __( 'Enabled', 'briteverify-gravityforms' ) : __( 'Disabled', 'briteverify-gravityforms' ) ); ?>
 					</label>
 					&nbsp;&nbsp;
 
-					<input type="radio" name="bv_gf_enable" id="bv_gf_enable_yes" size="10" value="yes" />
+					<input type="radio" name="bv_gf_enable" id="bv_gf_enable_yes" size="10" value="yes" onclick="return SetFieldProperty( 'bv_gf_enable', this.value );" onkeypress="return SetFieldProperty( 'bv_gf_enable', this.value );"/>
 					<label for="bv_gf_enable_yes" class="inline">
 						<?php esc_html_e( 'Enable', 'briteverify-gravityforms' ); ?>
 					</label>
 					&nbsp;&nbsp;
 
-					<input type="radio" name="bv_gf_enable" id="bv_gf_enable_no" size="10" value="no" />
+					<input type="radio" name="bv_gf_enable" id="bv_gf_enable_no" size="10" value="no" onclick="return SetFieldProperty( 'bv_gf_enable', this.value );" onkeypress="return SetFieldProperty( 'bv_gf_enable', this.value );"/>
 					<label for="bv_gf_enable_no" class="inline">
 						<?php esc_html_e( 'Disable', 'briteverify-gravityforms' ); ?>
 					</label>
@@ -71,19 +72,19 @@ class BV_GF_Field_Settings{
 				<label class="section_label"><?php esc_html_e( 'BriteVerify: Allow Disposable Email', 'briteverify-gravityforms' ); ?> </label>
 				<div>
 
-					<input type="radio" name="bv_gf_allow_disposable" id="bv_gf_allow_disposable_default" size="10" value="default" />
-					<label for="bv_gf_enable_default" class="inline">
+					<input type="radio" name="bv_gf_allow_disposable" id="bv_gf_allow_disposable_default" size="10" value="" onclick="return SetFieldProperty( 'bv_gf_allow_disposable', this.value );" onkeypress="return SetFieldProperty( 'bv_gf_allow_disposable', this.value );"/>
+					<label for="bv_gf_allow_disposable_default" class="inline">
 						<?php printf( esc_html__( 'Default (%s)', 'briteverify-gravityforms' ), 1 == get_option( 'bv_gf_allow_disposable' ) ? __( 'Allow', 'briteverify-gravityforms' ) : __( 'Do Not Allow', 'briteverify-gravityforms' ) ); ?>
 					</label>
 					&nbsp;&nbsp;
 
-					<input type="radio" name="bv_gf_allow_disposable" id="bv_gf_allow_disposable_yes" size="10" value="yes" />
+					<input type="radio" name="bv_gf_allow_disposable" id="bv_gf_allow_disposable_yes" size="10" value="yes" onclick="return SetFieldProperty( 'bv_gf_allow_disposable', this.value );" onkeypress="return SetFieldProperty( 'bv_gf_allow_disposable', this.value );"/>
 					<label for="bv_gf_allow_disposable_yes" class="inline">
 						<?php esc_html_e( 'Allow', 'briteverify-gravityforms' ); ?>
 					</label>
 					&nbsp;&nbsp;
 
-					<input type="radio" name="bv_gf_allow_disposable" id="bv_gf_allow_disposable_no" size="10" value="no" />
+					<input type="radio" name="bv_gf_allow_disposable" id="bv_gf_allow_disposable_no" size="10" value="no" onclick="return SetFieldProperty( 'bv_gf_allow_disposable', this.value );" onkeypress="return SetFieldProperty( 'bv_gf_allow_disposable', this.value );"/>
 					<label for="bv_gf_allow_disposable_no" class="inline">
 						<?php esc_html_e( 'Do Not Allow', 'briteverify-gravityforms' ); ?>
 					</label>
@@ -101,13 +102,35 @@ class BV_GF_Field_Settings{
 	public function field_editor_js(){
 		?>
 		<script type='text/javascript'>
-			//adding setting to fields of type "email"
+			/* Display setting only on "email" field type */
 			fieldSettings["email"] += ", .bv_gf_enable_field_setting, .bv_gf_allow_disposable_field_setting";
 
-			//binding to the load field settings event to initialize the checkbox
-			//jQuery(document).bind("gform_load_field_settings", function(event, field, form){
-				//jQuery("#field_encrypt_value").attr("checked", field["encryptField"] == true);
-			//});
+			/* Bind on "load field" to display saved data */
+			jQuery(document).bind("gform_load_field_settings", function( event, field, form ){
+				if( 'email' != field.type ){
+					return;
+				}
+				/* Enable */
+				if( 'yes' == field.bv_gf_enable ){
+					jQuery( '#bv_gf_enable_yes' ).prop( 'checked', true );
+				}
+				else if( 'no' ==  field.bv_gf_enable ){
+					jQuery( '#bv_gf_enable_no' ).prop( 'checked', true );
+				}
+				else{
+					jQuery( '#bv_gf_enable_default' ).prop( 'checked', true );
+				}
+				/* Allow Disposable */
+				if( 'yes' == field.bv_gf_allow_disposable ){
+					jQuery( '#bv_gf_allow_disposable_yes' ).prop( 'checked', true );
+				}
+				else if( 'no' ==  field.bv_gf_allow_disposable ){
+					jQuery( '#bv_gf_allow_disposable_no' ).prop( 'checked', true );
+				}
+				else{
+					jQuery( '#bv_gf_allow_disposable_default' ).prop( 'checked', true );
+				}
+			});
 		</script>
 		<?php
 	}
